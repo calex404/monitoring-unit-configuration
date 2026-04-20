7b55631ae5a8bfe3fb3d69da0ccbd8d0b570b622
Valéria <omintakeinenkeinenmakeinen@gmail.com>
2026-04-20 18:56:37 +0200

    Renaming variables.

--- database.py
+++ database.py
@@ -4,32 +4,35 @@ from influxdb_client import InfluxDBClient, Point
 from influxdb_client.client.write_api import SYNCHRONOUS
 from personal import *    
 
+
 topic = "senzory"
 bucket = "senzory"
 
+# InfluxDB connection
 influx = InfluxDBClient(url=influx_url, token=influx_token, org=influx_org)
-write_api = influx.write_api(write_options=SYNCHRONOUS)
+api = influx.write_api(write_options=SYNCHRONOUS)
 
 def zaznam(client, userdata, msg):
     try:
         data = json.loads(msg.payload.decode())
-        for sensor, hodnoty in data.items():
+        for sensor, measurement in data.items():
             point = Point("senzory") \
                 .tag("senzor", sensor)
-            for key, value in hodnoty.items():
+            for key, value in measurement.items():
                 point = point.field(key, value)
-            write_api.write(bucket=bucket, org=influx_org, record=point)
-            print(f"Záznam o: {sensor}")
+            api.write(bucket=bucket, org=influx_org, record=point)
+            print(f"Záznam o: {sensor} uložený.")
         print()
     except Exception as e:
         print("Error:", e)
 
-client = mqtt.Client()
-client.username_pw_set(mqtt_user, mqtt_password)
-client.tls_set()
-client.on_message = zaznam
-client.connect(mqtt_host, 8883)
-client.subscribe(topic)
+# MQTT connection
+broker = mqtt.Client()
+broker.username_pw_set(mqtt_user, mqtt_password)
+broker.tls_set()                                   # TLS encryption
+broker.on_message = zaznam
+broker.connect(mqtt_host, 8883)
+broker.subscribe(topic)
 
 print("Čakanie na dáta...")
-client.loop_forever()
+broker.loop_forever()
